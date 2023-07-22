fe7bd748a5b5ce45d6fbe49ed2106a3d4960756b
HEGAF <>
2023-07-22 10:59:46 +0700

    useState Commit [Week4-July-22-2023]

--- App.js
+++ App.js
@@ -3,14 +3,22 @@ import ViewBoxesWIthColorAndText from './components/ViewBoxesWIthColorAndText';
 import DisplayandImage from './components/DisplayandImage';
 import LotsOfGreeting from './components/LotsOfGreeting';
 import CustomText from './components/CustomText';
+import Counter from './components/Counter';
+import MyInput from './components/MyInput';
+import IncrementCounter from './components/IncrementCounter';
+import Form from './components/Form';
 
 export default function App() {
   return (
-    <View /* style={styles.container} */>
+    <View style={styles.container}>
       {/* <ViewBoxesWIthColorAndText/> */}
       {/* <DisplayandImage/> */}
       {/* <LotsOfGreeting/> */}
-      <CustomText/>
+      {/* <CustomText/> */}
+      {/* <Counter/> */}
+      {/* <MyInput/> */}
+      {/* {<IncrementCounter/>} */}
+      {<Form/>}
     </View>
   );
 }
---
+++ components/IncrementCounter.js
@@ -0,0 +1,33 @@
+import { Text, View, Button } from 'react-native'
+import React from 'react'
+import { useState } from 'react'
+
+const IncrementCounter = () => {
+
+    const [age, setAge] = useState(23);
+
+    const increment = ()=>{
+        setAge(a => a + 1);
+    }
+
+  return (
+    <View>
+      <Text style = {{fontSize:24,fontStyle:'bold'}}>Your age: {age + '\n\n'}</Text>
+      
+      <Button
+        title = '+3'
+        onPress={()=>setAge(age+3)}
+      />
+
+      <Text>{'\n'}</Text>
+
+      <Button
+        title = '+1'
+        onPress={increment}
+      />
+      
+    </View>
+  )
+}
+
+export default IncrementCounter
