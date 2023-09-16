64eb8571d3272975fd24b605ae0b9b4d737f1004
HEGAF <69428505+HEGAF@users.noreply.github.com>
2023-09-16 11:58:55 +0700

    second_commit

--- App.js
+++ App.js
@@ -1,48 +1,94 @@
-import {View, Text} from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
 
 import {NavigationContainer} from "@react-navigation/native"
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
-function HomeScreen() {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Home</Text>
+
+function Feed(){
+    return(
+        <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Feed</Text>
         </View>
     )
 }
 
-
-function SettingScreen() {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Settings!</Text>
+function Profile(){
+    return(
+        <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Profile</Text>
         </View>
     )
 }
 
+function Notification(){
+    return(
+        <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Notification</Text>
+        </View>
+    )
+}
 
-// HomeScreen และ SettingScreen ตามเดิม
+const Tab = createMaterialBottomTabNavigator();
 
-const Tab = createBottomTabNavigator();
+function MyTabs(){
+    return(
+        <Tab.Navigator
+            initialRouteName = 'Feed'
+            activeColor='#e91e63'
+            labelStyle = {{fontSize : 12}}
+            style = {{backgroundColor:'tomato'}}
+        >
+            <Tab.Screen
+                name = 'Feed'
+                component={Feed}
+                options={{
+                    tabBarLabel : 'Home',
+                    tabBarIcon : ({color})=>(
+                        <MaterialCommunityIcons name = 'home' color = {color} size={26}/>
+                    )
+                }}  
+            
+            />
 
-function MyTab() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name='Settings' component={SettingScreen}/>
+            <Tab.Screen
+                name = 'Notification'
+                component={Notification}
+                options={{
+                    tabBarLabel : 'Updates',
+                    tabBarIcon : ({color})=>(
+                        <MaterialCommunityIcons name = 'bell' color = {color} size={26}/>
+                    )
+                }}  
+            
+            />
+
+            <Tab.Screen
+                name = 'Profile'
+                component={Profile}
+                options={{
+                    tabBarLabel : 'Account',
+                    tabBarIcon : ({color})=>(
+                        <MaterialCommunityIcons name = 'account' color = {color} size={26}/>
+                    )
+                }}  
+            
+            />
+
         </Tab.Navigator>
     )
 }
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <MyTab />
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+        <MyTabs/>
+    </NavigationContainer>
+  )
 }
 
 export default App
- 
